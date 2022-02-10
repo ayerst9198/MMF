4a0a7508e4bcbee832926064bf40d7fc6dbe31e0
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2022-02-10 14:27:35 +1300

    Update 00_MMF_Base_Component_01.py

---
+++ 00_MMF_Base_Component_01.py
@@ -0,0 +1,29 @@
+# import statements
+
+
+# functions go here
+
+
+# *********** Main Routine ***********
+
+# Set up Dictionaries / lists needed to hold data
+
+# Ask user if they have used the program before & show instructions if they haven't
+
+# Loop to get ticket details
+
+    # Get name (can't be blank)
+
+    # Get age (between 12 and 130)
+
+    # Calculate ticket price
+
+    # Loop to ask for snacks
+
+    # Calculate snack price
+
+    # Ask for payment method
+
+    # Calculate total sales and profit
+
+# Outpuyt data to text file
