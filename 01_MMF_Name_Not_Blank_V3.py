818701cd7b90acd13af993a91f1426a0008dc703
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2022-02-11 12:51:52 +1300

    Create 01_MMF_Name_Not_Blank_V3.py

---
+++ 01_MMF_Name_Not_Blank_V3.py
@@ -0,0 +1,24 @@
+# functions go here
+
+def not_blank(question, error_msg):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response == "":
+            print(error_msg)
+        elif str.isspace(response):
+            print(error_msg)
+        else:
+            return response
+            
+
+# Main routine goes here
+
+while 1 == 1:
+    name = not_blank("name: ", "no blanks, sorry")
+    print()
+    print("Your name is:",name)
+    print()
+    print()
