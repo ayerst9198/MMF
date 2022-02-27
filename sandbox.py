069fcfa05d2e962663c6bcc26867beee00ba87e3
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2022-02-28 11:51:26 +1300

    Started string analyser

--- sandbox.py
+++ sandbox.py
@@ -1,30 +1,4 @@
-# functions go here
+my_string = "hello world"
 
-# taken from RPS
-def yes_no(question):
-    valid = False
-    while not valid:
-        response = input(question).lower()
-
-        if response == "yes" or response == "y":
-            response = "yes"
-            return response
-
-        elif response == "no" or response == "n":
-            response = "no"
-            return response
-
-        else:
-            print("Please input yes / no")
-
-# Main Routine Goes Here
-count = 0
-while 1 == 1:
-    want_snacks = yes_no("Do you want snacks? ")
-    print("Answer OK, you said:", want_snacks)
-    print()
-    if want_snacks == "yes":
-        count += 1
-    if count == 6:
-        print("You a greedy lil mf aren't ya")
-        break
+print("First letter: ", my_string[0])
+print("rest of string: ", my_string[1:])
